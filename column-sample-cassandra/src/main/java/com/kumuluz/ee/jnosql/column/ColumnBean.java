23c6ff8aaa907a9ecad63b985feebcc3a4e7b0cb
Klemen Kobau <klemen.kobau@gmail.com>
2019-08-16 13:15:04 +0200

    graph database config uses - instead of .

--- column-sample-cassandra/src/main/java/com/kumuluz/ee/jnosql/column/ColumnBean.java
+++ column-sample-cassandra/src/main/java/com/kumuluz/ee/jnosql/column/ColumnBean.java
@@ -1,27 +1,35 @@
 package com.kumuluz.ee.jnosql.column;
 
+import com.kumuluz.ee.jnosql.common.DatabaseLogic;
+import com.kumuluz.ee.jnosql.common.DatabaseTemplate;
 import org.jnosql.artemis.Database;
 import org.jnosql.artemis.DatabaseType;
-import org.jnosql.artemis.column.ColumnTemplate;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import java.util.Optional;
+import java.util.LinkedList;
+import java.util.List;
 
 @ApplicationScoped
 public class ColumnBean {
 
-	@Inject
-	private ColumnTemplate template;
+//	@Inject
+//	private ColumnTemplate template;
 
 	@Inject
 	@Database(DatabaseType.COLUMN)
 	private PersonRepository personRepository;
 
-	public Person getPersonById(long id) {
-		Optional<Person> entity = template.find(Person.class, id);
-		return entity.orElse(null);
-	}
+	@Inject
+	@DatabaseLogic(DatabaseType.COLUMN)
+	private DatabaseTemplate template;
+
+//	public Person getPersonById(long id) {
+//		Optional<Person> entity = template.find(Person.class, id);
+//		return entity.orElse(null);
+//	}
 
 	public Person savePerson(Person person) {
 		return template.insert(person);
@@ -30,4 +38,24 @@ public class ColumnBean {
 	public Long count() {
 		return personRepository.count();
 	}
+
+	private void init(@Observes @Initialized(ApplicationScoped.class) Object object) {
+		Person jan = new Person();
+		jan.setId(1L);
+		jan.setName("jan");
+
+		Person jan2 = new Person();
+		jan2.setId(2L);
+		jan2.setName("jan");
+
+		List<Person> personList = new LinkedList<>();
+		personList.add(jan);
+		personList.add(jan2);
+		Iterable<Person> out = template.insert(personList);
+
+		System.err.println(personRepository.count());
+		personRepository.deleteById(1L);
+		personRepository.deleteById(2L);
+	}
+
 }
--- column-sample-cassandra/src/main/java/com/kumuluz/ee/jnosql/column/ColumnResource.java
+++ column-sample-cassandra/src/main/java/com/kumuluz/ee/jnosql/column/ColumnResource.java
@@ -2,7 +2,10 @@ package com.kumuluz.ee.jnosql.column;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -16,12 +19,12 @@ public class ColumnResource {
 	@Inject
 	private ColumnBean columnBean;
 
-	@GET
-	@Path("{id}")
-	public Response getJohn(@PathParam("id") long id) {
-		Person john = columnBean.getPersonById(id);
-		return Response.ok(john).build();
-	}
+//	@GET
+//	@Path("{id}")
+//	public Response getJohn(@PathParam("id") long id) {
+//		Person john = columnBean.getPersonById(id);
+//		return Response.ok(john).build();
+//	}
 
 	@GET
 	public Response putJohn() {
