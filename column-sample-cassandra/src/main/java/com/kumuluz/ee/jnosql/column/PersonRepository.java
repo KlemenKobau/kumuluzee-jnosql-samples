364c75251ab3450b1921bf14e9e5895adcd5ec8c
Klemen Kobau <klemen.kobau@gmail.com>
2019-08-16 12:30:27 +0200

    using repositories

---
+++ column-sample-cassandra/src/main/java/com/kumuluz/ee/jnosql/column/PersonRepository.java
@@ -0,0 +1,6 @@
+package com.kumuluz.ee.jnosql.column;
+
+import org.jnosql.artemis.Repository;
+
+public interface PersonRepository extends Repository<Person, Long> {
+}
---
+++ graph-sample-neo4j-remote/src/main/java/com/kumuluz/ee/jnosql/samples/graph/NodeRepository.java
@@ -0,0 +1,12 @@
+package com.kumuluz.ee.jnosql.samples.graph;
+
+import org.jnosql.artemis.Query;
+import org.jnosql.artemis.Repository;
+
+import java.util.List;
+
+public interface NodeRepository extends Repository<Node, Long> {
+
+	@Query("g.v(0);")
+	public List<Node> getAllNodes();
+}
