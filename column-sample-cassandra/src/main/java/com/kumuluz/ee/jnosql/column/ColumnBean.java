a70e99c7d6d87c250e1519a69de0086e29602ed5
klemen kobau <klemen.kobau@gmail.com>
2020-07-12 17:48:23 +0200

    added license and author

--- column-sample-cassandra/src/main/java/com/kumuluz/ee/jnosql/column/ColumnBean.java
+++ column-sample-cassandra/src/main/java/com/kumuluz/ee/jnosql/column/ColumnBean.java
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (c) 2014-2017 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.kumuluz.ee.jnosql.column;
 
 import org.jnosql.artemis.Database;
@@ -6,14 +26,13 @@ import org.jnosql.artemis.PreparedStatement;
 import org.jnosql.artemis.column.ColumnTemplate;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.Initialized;
-import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 
+/**
+ * @author Klemen Kobau
+ */
 @ApplicationScoped
 public class ColumnBean {
 
--- graph-sample-neo4j-remote/src/main/java/com/kumuluz/ee/jnosql/samples/graph/NodeRepository.java
+++ graph-sample-neo4j-remote/src/main/java/com/kumuluz/ee/jnosql/samples/graph/NodeRepository.java
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (c) 2014-2017 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.kumuluz.ee.jnosql.samples.graph;
 
 import org.jnosql.artemis.Query;
@@ -5,8 +25,11 @@ import org.jnosql.artemis.Repository;
 
 import java.util.List;
 
+/**
+ * @author Klemen Kobau
+ */
 public interface NodeRepository extends Repository<Node, Long> {
 
 	@Query("g.V('0');")
-	public List<Node> getAllNodes();
+	List<Node> getAllNodes();
 }
